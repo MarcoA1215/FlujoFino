7467a842cf392744251209958ed848efb00e751d
Marco Avila <avilamarcodavid@gmail.com>
2026-09-15 13:34:45 -0400

    feat(frontend): remove 'Mover a Pendiente' button since it is fully automated

--- apps/frontend/src/pages/Orders.tsx
+++ apps/frontend/src/pages/Orders.tsx
@@ -303,11 +303,7 @@ const Orders: React.FC = () => {
                     {order.status !== OrderStatus.CANCELED && order.status !== OrderStatus.DELIVERED && (
                       <div className="ion-margin-top" style={{ display: 'flex', gap: '10px', flexWrap: 'wrap' }}>
                         
-                        {order.status === OrderStatus.PREPARING && (
-                          <IonButton style={{ flex: 1 }} color="warning" onClick={() => updateStatus(order.id, OrderStatus.PENDING)}>
-                            Mover a Pendiente
-                          </IonButton>
-                        )}
+                        
                         <IonButton style={{ flex: 1 }} color="success" onClick={() => updateStatus(order.id, OrderStatus.DELIVERED)}>
                           Entregar Pedido
                         </IonButton>
